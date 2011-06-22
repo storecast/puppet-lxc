3dda96993dab2c2e61bd39d4a8508bebcdd9320d
Thomas Clavier <tom@tcweb.org>
2011-06-22 11:54:36 +0200

    mount cgroup

--- manifests/controlling_host.pp
+++ manifests/controlling_host.pp
@@ -2,5 +2,17 @@ class puppet-lxc::controlling_host {
 	package { ["lxc", "lvm2", "reiserfsprogs", "bridge-utils", "debootstrap"]:
 		ensure => latest;
 	}
+
+	file { '/cgroup' : ensure => directory; }
+
+	mount {'mount_cgroup' : 
+		name => '/cgroup',
+		atbout => true,
+		device => 'cgroup',
+		ensure => mounted,
+		fstype => 'cgroup',
+		options => 'defaults',
+		remounts => false;
+	}
 }
 
