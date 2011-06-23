3df872d19c9b57c329448318b096eb6b4a43e2d1
Thomas Clavier <tom@tcweb.org>
2011-06-23 18:05:10 +0200

    test

--- manifests/controlling_host.pp
+++ manifests/controlling_host.pp
@@ -5,8 +5,8 @@ class puppet-lxc::controlling_host {
 
 	file { '/cgroup' : ensure => directory; }
 
-	file { '/usr/local/bin/build_vm': 
-		source  => "puppet:///modules/puppet-lxc/usr/local/bin/build_vm",
+	file {'/usr/local/bin/build_vm':
+		source  => "puppet:///modules/puppet-lxc/usr/local/bin/build_vm",
 		owner   => root,
 		group   => root,
 		mode    => 555;
