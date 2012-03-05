af7896efa08eb61ec4a64fba607068223e3f3429
Thomas Clavier <tom@tcweb.org>
2012-03-06 00:03:28 +0100

    add container configuration and creation from host

--- manifests/container.pp
+++ manifests/container.pp
@@ -1,4 +1,6 @@
+# the container itself could be configured by puppet
 class puppet-lxc::container {
+  # we must remove klogd to avoid bug in multy-read kernel messages
 	package { ["klogd"]: ensure => purged; }
 }
 
---
+++ manifests/vm.pp
@@ -0,0 +1,28 @@
+# defined container from host 
+class puppet-lxc::vm ( $ip, $mac, $passwd, $distrib ) {
+  file {
+    "/var/lib/lxc/${name}/preseed.cfg" :
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => template("puppet-lxc/preseed.cfg.erb");
+    "/var/lib/lxc/${name}/rootfs/etc/network/interfaces" :
+      owner     => "root",
+      group     => "root",
+      mode      => 0644,
+      require   => Exec ["create ${name} container"]
+      subscribe => Exec ["create ${name} container"]
+      content   => template("puppet-lxc/interface.erb");
+  }
+
+  exec {
+    "create ${name} container": 
+      command     => "/usr/lib/lxc/templates/lxc-debian --preseed-file=/var/lib/lxc/${name}/preseed.cfg -p /var/lib/lxc/${name} -n ${name}",
+      require     => File ["/var/lib/lxc/${name}/preseed.cfg"],
+      refreshonly => false,
+      creates     => "/var/lib/lxc/${name}/config"
+  }
+
+
+}
+
