a687bf5d6cb88eb2f00b97d5eb0a1eb053433520
Udo Waechter <udo.waechter@txtr.com>
2012-03-30 12:44:58 +0200

    ADMIN-414 updated classnames, made classes parameterized

--- manifests/vm.pp
+++ manifests/vm.pp
@@ -1,28 +1,36 @@
 # defined container from host 
-define lxc::vm ( $ip, $mac, $passwd, $distrib ) {
-  file {
-    "/var/lib/lxc/${name}/preseed.cfg" :
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => template("puppet-lxc/preseed.cfg.erb");
-    "/var/lib/lxc/${name}/rootfs/etc/network/interfaces" :
-      owner     => "root",
-      group     => "root",
-      mode      => 0644,
-      require   => Exec ["create ${name} container"],
-      subscribe => Exec ["create ${name} container"],
-      content   => template("puppet-lxc/interface.erb");
-  }
+define lxc::vm ($ip,
+	$mac,
+	$passwd,
+	$distrib,
+	$ensure = "present") {
+	File {
+		ensure => $ensure,
+	}
+	file {
+		"/var/lib/lxc/${name}/preseed.cfg" :
+			owner => "root",
+			group => "root",
+			mode => 0644,
+			content => template("puppet-lxc/preseed.cfg.erb") ;
 
-  exec {
-    "create ${name} container": 
-      command     => "/usr/lib/lxc/templates/lxc-debian --preseed-file=/var/lib/lxc/${name}/preseed.cfg -p /var/lib/lxc/${name} -n ${name}",
-      require     => File ["/var/lib/lxc/${name}/preseed.cfg"],
-      refreshonly => false,
-      creates     => "/var/lib/lxc/${name}/config"
-  }
-
-
+		"/var/lib/lxc/${name}/rootfs/etc/network/interfaces" :
+			owner => "root",
+			group => "root",
+			mode => 0644,
+			require => Exec["create ${name} container"],
+			subscribe => Exec["create ${name} container"],
+			content => template("puppet-lxc/interface.erb") ;
+	}
+	if $ensure == "present" {
+		exec {
+			"create ${name} container" :
+				command =>
+				"/usr/lib/lxc/templates/lxc-debian --preseed-file=/var/lib/lxc/${name}/preseed.cfg -p /var/lib/lxc/${name} -n ${name}",
+				require => File["/var/lib/lxc/${name}/preseed.cfg"],
+				refreshonly => false,
+				creates => "/var/lib/lxc/${name}/config"
+		}
+	}
 }
 
